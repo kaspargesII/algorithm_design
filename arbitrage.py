1aab0d5f0920b7154f100a71453dc36f051d0002
kasparges <kasf@itu.dk>
2023-11-26 15:36:14 +0100

    Changed my perspective on how to solve the problem. Researched Bellman ford and negative cycles

--- arbitrage.py
+++ arbitrage.py
@@ -9,6 +9,16 @@
 # as it is computationally more efficient for sparse graphs, as compared to 
 # adjacency matrices.
 
+# Upon further inspection this seems to be a DP problem about detecting negative cycles within the graoh. 
+# A negative cycle is a directed cycle, such that the sum of edge weights are negative. 
+
+# We can run Bellman-Ford algorithm V times to detect a negative cycle. If we detect a negative cycle, we can output 'Arbitrage'
+# Need to figure out how to convert the decimal numbers to a negative number, possibly? It might not be necessary, as the ratios are still
+# Stricly smaller ?? Otherwise we can say a positive number  1 > is > 1 and a negative number 0 < is < 1. 
+# It is definitely necessary! To convert the problem we make the weights into their negative logarithm.
+# Adding the logarithmic weights up during the bellman ford algorithm corresponds to multiplying currencies
+
+
 # Some arbitrary number of test cases, need to use sys.stdinput to determine when input stream is over
 #input for on test case:
 number_of_currencies = int(input()) # between 1 and 200
